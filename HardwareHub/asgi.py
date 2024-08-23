4356b2d769729f183cfbc6320d9223fa426718b8
Neha Gupta <nehabilbera@gmail.com>
2024-08-23 18:04:54 +0530

    modified project structure

--- HardwareHub/asgi.py
+++ HardwareHub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ShopOnline project.
+ASGI config for HardwareHub project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShopOnline.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HardwareHub.settings')
 
 application = get_asgi_application()
